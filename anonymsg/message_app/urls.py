aab3a76ad8f92018ccdb70111ee418d1b1dd8ca1
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-23 03:42:12 +0600

    Url configured for several endpoints

--- anonymsg/message_app/urls.py
+++ anonymsg/message_app/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/login/', views.user_login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.user_logout, name='logout'),
+    path('messages/', views.messages, name='messages'),
+    path('user/<str:username>/', views.send_message, name='send_message'),
+    path('success/', views.success, name='success'),
 ]
