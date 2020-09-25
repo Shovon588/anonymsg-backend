8672664c192c0afa3b2d0cd960a772549d46d324
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-25 20:01:23 +0600

    view added for random_message

--- anonymsg/message_app/views.py
+++ anonymsg/message_app/views.py
@@ -3,32 +3,41 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages as django_message
+from django.core.paginator import Paginator
 
-from django.http import HttpResponseRedirect
-from django.urls import reverse
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from . models import Message
+
+from random import randint
 
 # Create your views here.
 
 
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    message_count = len(Message.objects.filter(user=request.user))
+    return render(request, 'home.html', {'message_count': message_count})
 
 
 def signup(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user_obj = User.objects.create(username=username)
-        user_obj.set_password(password)
-        user_obj.save()
 
-        user = authenticate(username=username, password=password)
-        login(request, user)
+        try:
+            User.objects.get(username=username)
+            django_message.info(request, f"{username} is already taken.")
+        except:
+            user_obj = User.objects.create(username=username)
+            user_obj.set_password(password)
+            user_obj.save()
 
-        return redirect('home')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            return redirect('home')
 
 
     return render(request, 'signup.html')
@@ -56,16 +65,28 @@ def user_logout(request):
 
 @login_required
 def messages(request):
-    return render(request, 'messages.html')
+    messages = Message.objects.filter(user=request.user).order_by('-time')
+    pagi = Paginator(messages, 4)
+    page_number = request.GET.get('page')
+    page_obj = pagi.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj
+    }
+
+    return render(request, 'messages.html', context)
 
 
 def send_message(request, username):
+    if username == request.user.username:
+        return redirect('home')
+
     if request.method == 'POST':
         message = request.POST.get('message')
         try:
             user = User.objects.get(username=username)
-            Message.objects.create(user=user, message=message)
-            django_message.info(request, f"Message Sent to {username}.")
+            Message.objects.create(user=user, message=message, type="anonymous")
+            django_message.info(request, f"Message sent to {username}.")
             return redirect('success')
         except:
             django_message.info(request, "Invalid Target User")
@@ -79,3 +100,23 @@ def send_message(request, username):
 
 def success(request):
     return render(request, 'success.html')
+
+
+def random_message(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        users = User.objects.all()
+        random_index = randint(0, len(users)-1)
+        user = users[random_index]
+
+        if user==request.user:
+            if random_index == 0:
+                user = users[random_index + 1]
+            else:
+                user = users[random_index - 1]
+
+        Message.objects.create(user=user, message=message, type="random")
+        django_message.info(request, "Message sent to a random user.")
+        return redirect('success')
+
+    return render(request, 'random_message.html')
