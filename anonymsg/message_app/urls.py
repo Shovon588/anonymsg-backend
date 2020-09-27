5c6ecc93523006d6a0c34b562362076d2e686d95
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-27 22:52:22 +0600

    url configured for new views

--- anonymsg/message_app/urls.py
+++ anonymsg/message_app/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/', views.user_logout, name='logout'),
     path('messages/', views.messages, name='messages'),
+    path('favorite/', views.favorite_messages, name='favorite'),
     path('user/<str:username>/', views.send_message, name='send_message'),
     path('success/', views.success, name='success'),
     path('random/', views.random_message, name='random'),
+    path('delete/<str:id>', views.delete_message, name='delete'),
+    path('toggle-fav/<str:id>/', views.ToggleFavorite.as_view(), name='toggle-fav')
 ]
