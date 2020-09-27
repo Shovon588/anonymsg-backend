c493ff431e4dc4280b3abeb65f8b4b5993adabd2
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-27 22:47:30 +0600

    Modified model.
    Added a new field favorite, also added a method named when that returns when the message was sent

--- anonymsg/message_app/models.py
+++ anonymsg/message_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from datetime import datetime
 
 # Create your models here.
 
@@ -19,7 +20,53 @@ class Message(UserModel):
     ('annonymouse', 'Anonymous')
     )
     type = models.CharField(max_length=64, choices=TYPE_CHOICE, blank=True, null=True)
+    favorite = models.BooleanField(default=False)
     time = models.DateTimeField(default=timezone.now)
+
+    def when(self):
+        now = timezone.now()
+        diff = now - self.time
+        seconds = diff.seconds
+        tot_minutes = seconds // 60
+        tot_hours = tot_minutes // 60
+
+        if tot_hours > 20:
+            day = self.time.day
+            month = self.time.month
+            year = self.time.year
+            hour = self.time.hour
+            minute = self.time.minute
+
+            if hour > 12:
+                meridiem = "PM"
+                hour = hour-12
+            else:
+                meridiem = "AM"
+
+            ret_obj = f"{day}-{month}-{year} {hour}:{minute} {meridiem}"
+        else:
+            try:
+                minute = tot_minutes % (tot_hours*60)
+            except ZeroDivisionError:
+                minute = tot_minutes
+
+            if tot_hours == 0:
+                if minute == 1:
+                    ret_obj = "1 minute ago"
+                else:
+                    ret_obj = f"{minute} minutes ago"
+            elif tot_hours == 1:
+                if minute == 1:
+                    ret_obj = "1 hour 1 minute ago"
+                else:
+                    ret_obj = f"1 hour {minute} minutes ago"
+            else:
+                if minute == 1:
+                    ret_obj = f"{tot_hours} hours 1 minute ago"
+                else:
+                    ret_obj = f"{tot_hours} hours {minute} minutes ago"
+
+        return ret_obj
 
     def __str__(self):
         return self.message
