d4c28da69368333bb78ecd94f389d7cebaad6cb2
Md. Minul Islam Shovon <mainulislam588@gmail.com>
2020-09-28 21:17:34 +0600

    js function switched to traditional function convention
    ES6 was causing issue

--- anonymsg/message_app/static/js/master.js
+++ anonymsg/message_app/static/js/master.js
@@ -1,12 +1,12 @@
-copyLinkHandler = () => {
+function copyLinkHandler() {
   var copyText = document.getElementById("copy-link");
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   document.execCommand("copy");
   alert("Link copied to clipboard.");
-};
+}
 
-countCharHandler = () => {
+function countCharHandler() {
   message = document.getElementById("message").value;
   char_left = 256 - message.length;
   if (char_left < 25) {
@@ -18,17 +18,17 @@ countCharHandler = () => {
     document.getElementById("char-left").innerHTML =
       char_left + " characters left.";
   }
-};
+}
 
-modalHandler = (id) => {
+function modalHandler(id) {
   let message = document.getElementById("message-" + id).innerHTML;
   let header = document.getElementById("header-" + id).innerHTML;
 
   document.getElementById("exampleModalLongTitle").innerHTML = header;
   document.getElementById("modal-body").innerHTML = message;
-};
+}
 
-toggleFavorite = (id) => {
+function toggleFavorite(id) {
   let cname = document.getElementById("fav-" + id).className.split(" ")[1];
   if (cname === "fa-heart") {
     document.getElementById("fav-" + id).className = "fa fa-heart-o ml-3 mt-1";
@@ -37,8 +37,9 @@ toggleFavorite = (id) => {
       "fa fa-heart ml-3 mt-1 text-danger";
   }
 
-  let url = "http://127.0.0.1:8000/toggle-fav/" + id + "/";
-  fetch(url).then((response) => {
+  let local_url = "http://127.0.0.1:8000/toggle-fav/" + id + "/";
+  let twing_url = "https://twing.herokuapp.com/toggle-fav/" + id + "/";
+  fetch(twing_url).then((response) => {
     console.log(response.json());
   });
-};
+}
